a6cc74ef22ca032ea014291ecce2debb90f30d37
Kolbjørn Austreng <kolbjorn.austreng@radionor.no>
2026-01-08 11:59:40 +0100

    zsh: basic ergonomic improvements
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@radionor.no>

---
+++ python_repl_config.py
@@ -0,0 +1,18 @@
+# from _pyrepl.simple_interact import _get_reader
+# from _pyrepl.commands import up, down, left, right
+
+# reader = _get_reader()
+
+# reader.commands["slam_up"] = up
+# reader.commands["slam_down"] = down
+# reader.commands["slam_left"] = left
+# reader.commands["slam_right"] = right
+
+# reader.bind(r"\C-k", "slam_up")
+# reader.bind(r"\C-j", "slam_down")
+# reader.bind(r"\C-h", "slam_left")
+# reader.bind(r"\C-s", "slam_right")
+
+
+# next-history
+# previous-history
